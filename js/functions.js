9ee88f6f0a9d1f794c53c81a54158f6b417394fc
Vitaliy Sulimenko <prevetal@gmail.com>
2022-04-14 14:27:47 +0300

    Новая страница FAQ

--- js/functions.js
+++ js/functions.js
@@ -189,6 +189,24 @@ $(function () {
 	}
 
 
+	// Аккордион
+	$('body').on('click', '.accordion .accordion_item .head', function (e) {
+		e.preventDefault()
+
+		const $item = $(this).closest('.accordion_item'),
+			$accordion = $(this).closest('.accordion')
+
+		if ($item.hasClass('active')) {
+			$item.removeClass('active').find('.data').slideUp(300)
+		} else {
+			$accordion.find('.accordion_item').removeClass('active')
+			$accordion.find('.data').slideUp(300)
+
+			$item.addClass('active').find('.data').slideDown(300)
+		}
+	})
+
+
 	// Моб. меню
 	$('body').on('click', '.mob_header .mob_menu_link', function (e) {
 		e.preventDefault()
